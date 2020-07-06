2acc7a155838452791136275988d9a9aa27399b7
sashavg <sashavg@yandex.ru>
2020-07-06 22:10:38 +0300

    Add files via upload

---
+++ Lesson_03/Task_01.py
@@ -0,0 +1,61 @@
+
+# a = int(input('число1: '))
+# b = int(input('число2: '))
+# c = (int(a / b))
+# if c > 0
+#     print(c)
+# else:
+# print('на ноль дельть нельзя')
+# def my_div(num_1, num_2):
+#     try:
+#         num_1, num_2 = float(num_1), float(num_2)
+#         answer =
+
+
+
+# print(120)
+#
+# import math
+#
+# print(math.pi)
+
+
+# import  sys
+#
+# for i in sys.path:
+#     print(i)
+
+
+# import  sys
+# if 'help' in sys.argv or 'h' sys.argv:
+#     print()
+# else:
+#     _, path1, path2 = sys.argv:
+#     print({path1} {path2})
+#     time.sleep(10)
+#     print('копирование завершина')
+from hashlib import new
+import  os #  операционная система
+#
+# print(os.getcwd()) # вызов функции
+#
+ os.mkdir()  # создание новой папки
+
+# my_list = [1, 2, 3, 4, 5]
+# new_list = [el for el in my_list if el % 2 == 0]
+# print(new_list)
+#
+# new_dict = {el: el ** 2 for el in my_list if el % 2 == 0}
+#
+# print(new_dict)
+
+
+# import random
+#
+#  print(random.random())
+#
+#  print(random.randint(0, 10))
+#  random.seed(8) # фиксирует рандомное число
+#  print(random.randrange(0, 10, 2)) # рандомное число от 0 до 10 с шагом 2
+
+
